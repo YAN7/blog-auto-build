ede5cf9df9406b6d65b7532a896afe8326a7e5df
YAN7 <904394373@qq.com>
2019-05-01 00:53:12 +0800

    完成初版

---
+++ index.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const spawm = require('child_process').spawm;
+const createHeadler = require('github-webhook-handler');
+const config = require('./config');
+const handler = createHeadler({path: '/autoBuild', secret: config.secret})
+
+http.createServer((req, res) => {
+    handler(req, res, function (err) {
+        res.statusCode = 404;
+        res.end('there is something wrong!');
+    })
+}).listen(config.port);
+
+handler.on('error', function (err) {
+    console.log('Error:', err.message);
+})
+
+handler.on('push', function (event) { 
+    runCommand('sh', ['./autoBuild.sh'], function (txt) {
+        console.log(txt);
+    })
+})
+
+function runCommand(cmd, args, callback) {
+    const child = spawm(cmd, args);
+    let resp = '';
+    child.stdout.on('data', function (buffer) {
+        resp += buffer.toString();
+    })
+    child.stdout.on('end', function () {
+        callback(resp);
+    })
+
+}
